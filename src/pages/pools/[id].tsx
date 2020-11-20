955496f572980b6ec4d5444d9b6f01554bd45c1e
Gabriel Nascimento <gabriel.nbds@gmail.com>
2020-11-20 14:27:11 -0300

    Changed api Url

--- src/pages/pools/[id].tsx
+++ src/pages/pools/[id].tsx
@@ -35,7 +35,7 @@ export default function Candidates({ id, title, staticCandidates }: PoolProps) {
   }, [])
 
   useEffect(() => {
-    const socket = io(process.env.REACT_APP_API_URL)
+    const socket = io(process.env.NEXT_PUBLIC_API_URL)
 
     socket.on(id, (votedCandidate: ICandidate) => {
       const updatedCandidates = ref.current.map<ICandidate>(candidate => {
